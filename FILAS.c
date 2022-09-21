24909a904a87be491c35abc05d6d3da77132e8f0
Rafael Perina <rafael.prn3@gmail.com>
2022-09-21 01:19:15 -0300

    initial commit

---
+++ FILAS.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No{
+    int chave;
+    struct No *prox;
+}Fila;
+
+Fila *alocaNo(int k);
+void removeItem(Fila **p);
+void insereItem(Fila **p, int k);
+int Vazia(Fila *p);
+void LiberaFila(Fila **p);
+void imprime(Fila *p);
+
+int main(){
+    
+    return 0;
+}
+
+//FUNÇÔES
+Fila *alocaNo(int k){
+    Fila *novo = (Fila *)malloc(sizeof(Fila));
+    if(novo == NULL){
+        return NULL;
+    }
+    novo->chave = k;
+    novo->prox = NULL;
+    return novo;
+}
+
+void removeItem(Fila **p){ //remoção de item na fila, remove o primeiro item
+    if(*p == NULL){
+        printf("Fila vazia!\n");
+        return;
+    }
+    Fila *aux = *p; //vamos usar aux para remover
+    *p = (*p)->prox; //faz p olhar para o segundo item pq vamos remover o primeiro
+    free(aux);
+}
+
+void insereItem(Fila **p, int k){//inserção de item na fila, adiciona no fim da fila
+    if(*p == NULL){
+        printf("Fila vazia!\n");
+        return;
+    }
+    Fila *aux = *p, *novo = alocaNo(k); //aux percorre, novo aloca
+    while(aux->prox != NULL){
+        aux = aux->prox;
+    }
+    aux->prox = novo;
+}
+
+int Vazia(Fila *p){//verifica se a fila esta vazia
+    if(p != NULL){//se nao esta vazia
+        printf("Nao esta vazia");
+        return 0;
+    }
+    printf("A Fila esta vazia!");
+    return 1;
+}
+
+void LiberaFila(Fila **p){
+     if (*p == NULL){
+        return;
+    }
+    LiberaFila(&(*p)->prox);
+    (*p)->prox = NULL;
+    free(*p);
+    *p = NULL;
+}
+
+void imprime(Fila *p){
+    if(p == NULL){
+        printf("A Fila esta vazia!");
+        return;
+    }
+    while(p != NULL){
+        printf("\n%d", p->chave);
+        p = p->prox;
+    }
+}
+
---
+++ PILHAS.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No{
+    int chave;
+    struct No *prox;
+}Pilha;
+
+Pilha *alocaNo(int k);
+void removeItem(Pilha **p);
+void insereItem(Pilha **p, int k);
+int Vazia(Pilha *p);
+void LiberaPilha(Pilha **p);
+void imprime(Pilha *p);
+
+int main(){
+
+    return 0;
+}
+
+//FUNÇÕES
+Pilha *alocaNo(int k){
+    Pilha *novo = (Pilha *)malloc(sizeof(Pilha));
+    if(novo == NULL){
+        return NULL;
+    }
+    novo->chave = k;
+    novo->prox = NULL;
+    return novo;
+}
+
+void removeItem(Pilha **p){ //remoção de item na Pilha, remove o primeiro item
+    if(*p == NULL){
+        printf("Pilha vazia!\n");
+        return;
+    }
+    Pilha *aux = *p; //vamos usar aux p remover
+    *p = (*p)->prox; //faz p olhar para o segundo item pq vamos remover o primeiro
+    free(aux);
+}
+
+void insereItem(Pilha **p, int k){//inserção de item na Pilha, adiciona no inicio (topo) da Pilha
+    if(*p == NULL){
+        printf("Pilha vazia!\n");
+        return;
+    }
+    Pilha *aux = *p, *novo = alocaNo(k); //aux percorre, novo aloca
+    novo->prox = *p;
+    *p = novo;
+}
+
+int Vazia(Pilha *p){//verifica se a lista esta vazia
+    if(p != NULL){//se nao esta vazia
+        printf("Nao esta vazia");
+        return 0;
+    }
+    printf("A Pilha esta vazia!");
+    return 1;
+}
+
+void LiberaPilha(Pilha **p){
+     if (*p == NULL){
+        return;
+    }
+    LiberaPilha(&(*p)->prox);
+    (*p)->prox = NULL;
+    free(*p);
+    *p = NULL;
+}
+
+void imprime(Pilha *p){
+    if(p == NULL){
+        printf("A Pilha esta vazia!");
+        return;
+    }
+    while(p != NULL){
+        printf("\n%d", p->chave);
+        p = p->prox;
+    }
+}
+
